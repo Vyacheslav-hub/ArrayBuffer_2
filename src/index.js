7ef383bc2a108c880e8762c9fee122cceaca8e03
Vyacheslav <slavik.kurganov.26@mail.ru>
2026-04-28 18:46:08 +0300

    Добавил в файл index.js для билда класс ArrayBufferConverter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import {Magician} from "./Magician.js";
 import {Daemon} from "./Daemon.js";
+import {ArrayBufferConverter} from "./ArrayBufferConverter.js";
 
 new Magician(100, 2);
 new Daemon(100, 3);
+new ArrayBufferConverter();
